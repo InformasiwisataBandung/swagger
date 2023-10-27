da53042ab4e3fe106ef941b9bc6c8d67f4feda82
AkbarHasballah <105421566+AkbarHasballah@users.noreply.github.com>
2023-10-28 00:00:07 +0700

    update

---
+++ swaggerfunction.js
@@ -0,0 +1,22 @@
+import SwaggerUIBundle from 'https://cdn.skypack.dev/swagger-ui-dist/swagger-ui-bundle.js';
+
+
+
+export let URLData = "./openapi.yaml";
+
+export const UIData = SwaggerUIBundle({
+    url: URLData, //Location of Open API spec in the repo
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
+    ],
+    plugins: [
+        SwaggerUIBundle.plugins.DownloadUrl
+    ],
+});
+
+export function setSwagger() {
+    return UIData
+}
